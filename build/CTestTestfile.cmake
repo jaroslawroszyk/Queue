dc94416d14fbf5fb263384b3585b54f414d103b8
Jarosław Roszyk <48736575+jaroslawroszyk@users.noreply.github.com>
2022-01-02 02:30:14 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/private/NaukaZmackiem/Queue
-# Build directory: /mnt/c/private/NaukaZmackiem/Queue/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "kolejka/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/private/NaukaZmackiem/Queue/CMakeLists.txt;12;add_test;/mnt/c/private/NaukaZmackiem/Queue/CMakeLists.txt;0;")
-subdirs("kolejka")
