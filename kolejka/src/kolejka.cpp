6da7d5c0594d5aad0a882ce2f51a37dc931201a9
jaroslawroszyk <roszyk.jarek@gmail.com>
2022-01-02 03:23:37 +0100

    queue

--- kolejka/src/kolejka.cpp
+++ kolejka/src/kolejka.cpp
@@ -26,6 +26,40 @@ Queue::Queue(std::initializer_list<std::string> strList)
     }
 }
 
+Queue::Queue(const Queue &rhs) : capacity(rhs.capacity), tab(nullptr), count(rhs.count), begin(rhs.begin)
+{
+    tab = new std::string[capacity];
+    for (int i = 0; i < count; ++i)
+    {
+        tab[i] = rhs.tab[i];
+    }
+}
+
+Queue::Queue(Queue &&rhs) noexcept: capacity(rhs.capacity), tab(rhs.tab), count(rhs.count), begin(rhs.begin)
+{
+    rhs.tab = nullptr;
+}
+
+Queue &Queue::operator=(const Queue &rhs)
+{
+    if (this == &rhs)
+        return *this;
+    if (capacity != rhs.capacity and count != rhs.count) // czy begin -> begin != rhs.begin
+    {
+        delete[]tab;
+        tab = nullptr;
+        capacity = 0;
+        count = 0;
+        begin = 0;
+        tab = new std::string[rhs.capacity];
+        capacity = rhs.capacity;
+        count = rhs.count;
+        begin = rhs.begin;
+    }
+    std::copy(rhs.tab, rhs.tab + rhs.capacity, tab);
+    return *this;
+}
+
 Queue::~Queue()
 {
     delete[]tab;
@@ -59,31 +93,3 @@ auto Queue::pop() -> std::string
     begin %= capacity;
     return result;
 }
-
-Queue::Queue(const Queue &rhs) : capacity(rhs.capacity), tab(nullptr), count(rhs.count), begin(rhs.begin)
-{
-    tab = new std::string[capacity];
-    for (int i = 0; i < count; ++i)
-    {
-        tab[i] = rhs.tab[i];
-    }
-}
-
-Queue::Queue(Queue &&rhs) noexcept: capacity(rhs.capacity), tab(rhs.tab), count(rhs.count), begin(rhs.begin)
-{
-    rhs.tab = nullptr;
-}
-
-//Queue &Queue::operator=(const Queue &rhs) : capacity(rhs.capacity),tab(nullptr), count(rhs.cout),begin(rhs.begin()
-//{
-//    if (this == &rhs)
-//        return *this;
-//    if (capacity != rhs.capacity)
-//    {
-//        delete[]tab;
-//        tab = new std::string[rhs.capacity];
-//    }
-//    std::copy(rhs.tab,rhs.tab + rhs.capacity, tab);
-//    return *this;
-//}
-
